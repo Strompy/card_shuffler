28ea4289a7ab00d7b1ef28e110bbfede4e1fee21
Phillip Strom <phillip.strom@gmail.com>
2021-05-17 15:09:47 -0600

    Fix spacing

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Card, type: :model do
   describe 'Validations' do
-    it {should validate_presence_of :rank}
-    it {should validate_presence_of :suit}
-    it {should validate_presence_of :value}
+    it { should validate_presence_of :rank }
+    it { should validate_presence_of :suit }
+    it { should validate_presence_of :value }
   end
 end
