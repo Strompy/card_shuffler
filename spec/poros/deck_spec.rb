a99931c2272f7fae38b2c86a517a2637adfc7c15
Phillip Strom <phillip.strom@gmail.com>
2021-05-18 16:41:42 -0600

    Create shuffle method spec

--- spec/poros/deck_spec.rb
+++ spec/poros/deck_spec.rb
@@ -6,10 +6,20 @@ RSpec.describe Deck, type: :poro do
   end
 
   it 'can be created with 52 cards' do
-    # Rails.application.load_seed
     deck = Deck.new
     expect(deck).to be_instance_of(Deck)
-    expect(deck.cards.size).to eq 52
-    expect(deck.cards.uniq).to eq deck.cards
+    expect(deck.cards.size).to eq(52)
+    expect(deck.cards.uniq).to eq(deck.cards)
+  end
+
+  describe 'instance methods' do
+    it 'can shuffle the deck' do
+      deck = Deck.new
+      expect(deck.cards.size).to eq(52)
+      og_cards = deck.cards.clone
+      deck.shuffle
+      expect(deck.cards).to_not eq(og_cards)
+      expect(deck.cards.size).to eq(52)
+    end
   end
 end
