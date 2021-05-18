21e8eba398725e679352d64ad5ba5152682b9a88
Phillip Strom <phillip.strom@gmail.com>
2021-05-18 16:41:53 -0600

    Pass spec with inbuilt shuffle

--- app/poros/deck.rb
+++ app/poros/deck.rb
@@ -2,6 +2,11 @@ class Deck
   attr_reader :cards
 
   def initialize
-    @cards = Card.all
+    @cards = Card.all.to_a
+  end
+
+  def shuffle
+    require "pry"; binding.pry
+    cards.shuffle!
   end
 end
