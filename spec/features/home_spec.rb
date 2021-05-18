d95995f49d142ba98125a0feb42f27ae01a413b4
Phillip Strom <phillip.strom@gmail.com>
2021-05-18 15:59:21 -0600

    Add assertions for initial page visit

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'When a user visits the home page' do
-  describe 'it displays a new ushuffled deck' do
-    visit '/'
+  it 'it displays a new ushuffled deck' do
+    visit root_path
+    expect(page).to have_content('Card Shuffler')
+    expect(page).to have_css('.card', count: 52)
+    find('.card', match: :first) do
+      expect(page).to have_content('2')
+      expect(page).to have_content('hearts')
+    end
+    all('.card').last do
+      expect(page).to have_content('A')
+      expect(page).to have_content('clubs')
+    end
+    # expect(page).to have_button('Shuffle')
   end
 end
