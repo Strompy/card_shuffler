31556e38a93d3f0964e1337095224345a9512b10
Phillip Strom <phillip.strom@gmail.com>
2021-05-18 16:41:35 -0600

    Add shuffle params to controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
     @deck = Deck.new
+    if params[:shuffle]
+      require "pry"; binding.pry
+      @deck.shuffle
+    end
   end
+
 end
